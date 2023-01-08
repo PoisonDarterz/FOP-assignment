f0a37b5b4a6fcedf00b16a02793d009a6247e0fe
PoisonDarterz <jc.chuan.0303@gmail.com>
2023-01-08 19:45:10 +0800

    test discord ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing discord ping");
+        System.out.println("adding my own branch");
     }
 }
